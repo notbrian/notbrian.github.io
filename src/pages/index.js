7d51dfdb0c6ca65388b4f50cf00668432b032cbf
Brian Nguyen <nguyen.brian70@gmail.com>
2019-10-18 23:31:30 -0400

    Initial Gatsby base

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import styled from "styled-components"
+
+const MainFlex = styled.div``
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1> Brian Nguyen </h1>
+  </Layout>
+)
+
+export default IndexPage
